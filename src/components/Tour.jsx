7beead5a3c2229d664647f569750bc3e83d1503c
Yogender <yashmittal92002@gmail.com>
2025-08-18 00:15:25 +0530

    changes

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -11,13 +11,13 @@ const Tour = ({ id, image, info, price, name, removeTour }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>
+        <p id={`tour-item-para-${id}`}>
           {readMore ? info : `${info.substring(0, 200)}...`}
           <button onClick={() => setReadMore(!readMore)}>
             {readMore ? "Show Less" : "Show More"}
           </button>
         </p>
-        <button className="delete-btn" onClick={() => removeTour(id)}>
+        <button id={`delete-btn-${id}`} className="delete-btn" onClick={() => removeTour(id)}>
           Not Interested
         </button>
       </footer>
